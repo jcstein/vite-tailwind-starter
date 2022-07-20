9f27ae422609d77248bd17241e1bcaafbd3a43a5
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-20 15:34:57 -0400

    sun for rainbow

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="bg-zinc-900 flex justify-center items-center h-screen text-white w-screen p-10">
       <div>
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl pb-10 font-bold text-center">
-          gm 🌈{" "}
+          gm ☀️{" "}
           <a
             href="https://vitejs.dev"
             className="underline hover:text-[#356DF3]"
